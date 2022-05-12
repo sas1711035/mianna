65040c21b6e99fdc5d6a86dc2be22283c74a7d38
sas1711035 <105028848+sas1711035@users.noreply.github.com>
2022-05-12 11:01:08 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+music.setVolume(255)
 basic.forever(function () {
-	
+    music.playTone(988, music.beat(BeatFraction.Half))
+    basic.pause(100)
+    music.playTone(988, music.beat(BeatFraction.Half))
+    basic.pause(100)
 })
